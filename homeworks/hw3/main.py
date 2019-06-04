1720bde8b30dc2d31643cbca9557e0348887f4ed
Carlos Souza <souza@gatech.edu>
2019-06-04 17:39:22 -0300

    Adding first tentative to solve HW3

--- homeworks/hw3/main.py
+++ homeworks/hw3/main.py
@@ -1,15 +1,61 @@
 import numpy as np
 import json
+import tester
+
+
+def generate_mdp(num_states=3, num_actions=2, num_transitions=2, gamma=0.75):
+    #l = np.arange(num_states)
+    #print(l)
+    #c = np.random.choice(l, 2)
+    #print(c)
+    #d = np.setdiff1d(l, c)
+    #print(d)
+
+    mdp = dict()
+    mdp['gamma'] = gamma
+    mdp['states'] = list()
+
+    for s in range(num_states):
+        state = dict()
+        state['id'] = s
+        state['actions'] = list()
+        for a in range(num_actions):
+            action = dict()
+            action['id'] = a
+            action['transitions'] = list()
+            possible_states = np.arange(num_states)
+            for t in range(num_transitions):
+                transition = dict()
+                transition['id'] = t
+                transition['probability'] = 0.5
+                transition['reward'] = np.random.randint(-1, 2)
+                transition['to'] = int(np.random.choice(possible_states))
+                action['transitions'].append(transition)
+                possible_states = np.setdiff1d(possible_states,
+                                               np.array(transition['to']))
+
+            state['actions'].append(action)
+
+        mdp['states'].append(state)
+
+    return mdp
+
+    #j = json.dumps(mdp, indent=4)
+    #print(j)
+
 
 if __name__ == '__main__':
-    d = dict()
-    d['gamma'] = 0.75
-    d['states'] = list()
-    d['states'].append('a')
-    d['states'].append('b')
-    d['states'].append('c')
-    print(d)
+    #with open('tester/sample.json') as data_file:
+    #    mdp = json.load(data_file)
+    #niterations = tester.get_iterations_with_mdptoolbox(mdp)
+    #print(niterations)
 
-    j = json.dumps(d, indent=4)
-    print(j)
+    mdp = generate_mdp(num_states=30)
+    j = json.dumps(mdp, indent=4)
+    #print(j)
+
+    niterations = tester.get_iterations_with_mdptoolbox(mdp)
+    print(niterations)
+
+
 
---
+++ homeworks/hw3/tester/__init__.py
@@ -0,0 +1,1 @@
+from .hw3_tester import *
