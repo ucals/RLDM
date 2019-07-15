13bc4634c68c8cd7bf7c2aaaa3fda415123f59b0
Carlos Souza <souza@gatech.edu>
2019-07-15 18:04:30 -0300

    Adding Foe-Q (not working)

--- project_3/main.py
+++ project_3/main.py
@@ -5,10 +5,13 @@ from collections import defaultdict
 
 
 class MultiQ(object):
-    def __init__(self, selection='Foe-Q', alpha=0.001, gamma=0.9):
+    def __init__(self, selection='Foe-Q', alpha0=0.1, alpha_min=0.001,
+                 alpha_decay=0.999, gamma=0.9):
         self.env = environment.Soccer()
         self.selection = selection
-        self.alpha = alpha
+        self.alpha0 = alpha0
+        self.alpha_min = alpha_min
+        self.alpha_decay = alpha_decay
         self.gamma = gamma
         self.Q0 = defaultdict(lambda: np.zeros((self.env.num_actions,
                                                 self.env.num_actions)))
@@ -59,12 +62,20 @@ class MultiQ(object):
 
         return a0, a1
 
-    def train(self, num_episodes=1000, render=False, epsilon_decay=0.99,
+    def train(self, num_episodes=10000, render=False, epsilon_decay=0.999,
               min_epsilon=0.05, print_same_line=True):
+        state_s = (0, 1), (0, 2), 0
+        state_s_visits = 0
+        state_s_actions = ()
+        error = 0.
+
         epsilon = 1.0
         t = 0
         prob_actions0 = None
         prob_actions1 = None
+        wins = [0, 0]
+
+        alpha = self.alpha0
 
         for i_episode in range(num_episodes):
             state = self.env.reset()
@@ -74,23 +85,40 @@ class MultiQ(object):
                 if render:
                     self.env.render()
 
+                if state == state_s:
+                    state_s_actions = (a0, a1)
+                    if (a0 == 0) and (a1 == 3):
+                        Q1_old = self.Q1[state][a0][a1]
+                        state_s_visits += 1
+
                 next_state, rewards, done, info = self.env.step(a0, a1)
 
                 prob_actions0, v0 = self.minimax(self.Q0[next_state])
-                self.Q0[state][a0][a1] = (1 - self.alpha) * self.Q0[state][a0][a1] + self.alpha * ((1 - self.gamma) * rewards[0] + self.gamma * v0)
+                self.Q0[state][a0][a1] = (1 - alpha) * self.Q0[state][a0][a1] + alpha * ((1 - self.gamma) * rewards[0] + self.gamma * v0)
 
                 prob_actions1, v1 = self.minimax(self.Q1[next_state], maximin=True)
-                self.Q1[state][a0][a1] = (1 - self.alpha) * self.Q1[state][a0][a1] + self.alpha * ((1 - self.gamma) * rewards[1] + self.gamma * v1)
+                self.Q1[state][a0][a1] = (1 - alpha) * self.Q1[state][a0][a1] + alpha * ((1 - self.gamma) * rewards[1] + self.gamma * v1)
+                if (state == state_s) and (a0 == 0) and (a1 == 3):
+                    error = abs(self.Q1[state][a0][a1] - Q1_old)
+
+                # Compute statistics
+                if rewards[0] == 100:
+                    wins[0] += 1
+                elif rewards[1] == 100:
+                    wins[1] += 1
 
                 # Log data in-screen
                 if t > 0 and print_same_line:
-                    sys.stdout.write(b'\033[2A'.decode())
+                    sys.stdout.write(b'\033[4A'.decode())
 
-                print(f'Episode {i_episode + 1:>4}, ε: {epsilon:0.3f}, t: {t}\n'
-                      f'Actions: {(a0, a1)}, Rewards: {rewards}, Done: {done}, Info: {info}')
+                print(f'Episode {i_episode + 1:>4}, ε: {epsilon:0.3f}, α: {alpha:0.4f}, t: {t}, Wins: {wins}\n'
+                      f'Actions: {(a0, a1)}, Rewards: {rewards}, Done: {done}, Info: {info}\n'
+                      f'Error: {error:0.4f}, Visits in S state: {state_s_visits}, State S actions: {state_s_actions}\n'
+                      f'Prob actions 0: {prob_actions0}, Prob actions 1: {prob_actions1}')
 
                 state = next_state
                 epsilon = max(min_epsilon, epsilon * epsilon_decay)
+                alpha = max(self.alpha_min, alpha * self.alpha_decay)
                 a0, a1 = self.choose_actions(epsilon, prob_actions0, prob_actions1)
                 t += 1
 
